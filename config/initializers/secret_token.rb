fc3331978a11e4700c834d5ca5c69be42f0cd8d7
Pedro Alvarado Rincon <pwalvarado@gmail.com>
2015-02-26 10:01:19 -0500

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ResqueSchedulerExampleEdge::Application.config.secret_token = 'caa72a6e21477ba02f1f7af4886d6dd203da25da7489120258477541e03520fd15a5df57a58b5c256560d024c551ef3508aeed2038159febcd324061f23d24ec'
